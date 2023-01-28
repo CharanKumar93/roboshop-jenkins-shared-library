9d2808d68bf410b5c07fc7c830b82527de8cee7b
Charan <gcharankumar99@gmail.com>
2023-01-28 09:13:16 -0600

    vars file

--- vars/test1.groovy
+++ vars/test1.groovy
@@ -13,18 +13,19 @@ def new1() {
     def x = 2
     def y = 0
     while(x > y) {
-       println "${y}"
-       y++
+        println "${y}"
+        y++
     }
 
     for(int i = 0;i<5;i++) {
-      println(i);
+        println(i);
     }
 
     def fruits = ["apple", "banana", "orange"];
 
-    for (i in fruits) {
+    for(i in fruits) {
         println(i);
     }
-}
 
+
+}
