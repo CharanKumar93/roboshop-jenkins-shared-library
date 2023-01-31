4cd0ac96e4619c334d9b7f882dcd7e7d3dbb4838
Charan <gcharankumar99@gmail.com>
2023-01-31 14:04:37 -0600

    vars file

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -9,7 +9,7 @@ def call() {
 
             stage('Checkout') {
                 cleanWs()
-                git branch: 'main', url: "https://github.com/raghudevopsb70/${component}"
+                git branch: 'main', url: "https://github.com/charankumar93/${component}"
             }
 
             stage('Compile/Build') {
