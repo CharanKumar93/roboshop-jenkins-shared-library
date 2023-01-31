c72b6e242f50d63807fd637dda05562a6472a3c8
Charan <gcharankumar99@gmail.com>
2023-01-31 12:38:04 -0600

    vars file

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -28,12 +28,16 @@ def call() {
                     environment {
                         SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | ' +
                                 'sed \'s/"//g\')'
-                        SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | ' +
+                        //SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | ' +
                                 'sed \'s/"//g\')'
                     }
                     steps {
                         script{
-                            wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var:'PASSWORD']]]) {
+                            SONAR_PASS = sh ( script: aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | ' +
+                                    'sed \'s/"//g\'', returnStdout: true).trim()
+                            wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var:'SECRET']]]) {
+                                println "Password = ${SONAR_PASS}"
+                                sh "echo sh password = ${SONAR_PASS}"
                                 sh "sonar-scanner -Dsonar.host.url=http://172.31.2.132:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
                             }
                         }
@@ -53,10 +57,4 @@ def call() {
     } catch(Exception e) {
       common.email("Failed")
     }
-}
-
-wrap([$class: 'MaskPasswordsBuildWrapper',
-      varPasswordPairs: [[password: MASKED_SECRET]]]) {
-    echo 'Retrieve Secret: ' +  MASKED_SECRET
-    echo MASKED_SECRET
 }
