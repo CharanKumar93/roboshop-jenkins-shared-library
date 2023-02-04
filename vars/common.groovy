3a6451d04195e2162592bd6c9c6ce65b91892e5f
Charan <gcharankumar99@gmail.com>
2023-02-03 19:54:53 -0600

    vars file

--- vars/common.groovy
+++ vars/common.groovy
@@ -32,3 +32,10 @@ def email(email_note) {
   mail bcc: '', body: 'Job Failed - ${JOB_BASE_NAME}\nJenkins URL - ${JOB_URL}', cc: '', from: 'charanraj469@gmail.com', replyTo: '', subject: "Jenkins Job Failed - ${JOB_BASE_NAME}", to: 'charanraj469@gmail.com'
 }
 
+def artifactPush() {
+  if (app_lang == "nodejs") {
+    sh "zip -r cart-${TAG_NAME}.zip node_modules server.js"
+  }
+  sh 'ls -l'
+}
+
